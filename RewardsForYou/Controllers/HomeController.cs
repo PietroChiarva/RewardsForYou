18278480c1aca35651613132d4e2ab1dcad4b4c5
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-02-19 14:35:14 +0100

    azure login

--- RewardsForYou/Controllers/HomeController.cs
+++ RewardsForYou/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace RewardsForYou.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,19 +14,5 @@ namespace RewardsForYou.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
