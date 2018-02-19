8d132ff72f3068d7ba5be102a12128a506a165d3
NkompChristian <christian.nkomp@edu.itspiemonte.it>
2018-02-19 10:39:48 +0100

    login

---
+++ RewardsForYou/Models/SearchRewardsforyou.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RewardsForYou.Models
+{
+    public class SearchRewardsforyou : Users
+    {
+        public List<Users> ResultList { get; set; }
+
+        public new int? UsersID { get; set; }
+
+    }
+}
+
