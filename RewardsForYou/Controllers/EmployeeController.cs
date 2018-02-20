1d4a74d58ad2963fda53a7f532edcd6c782cc590
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-02-20 14:12:46 +0100

    employee first commit

--- RewardsForYou/Controllers/EmployeeController.cs
+++ RewardsForYou/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RewardsForYou.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,17 @@ namespace RewardsForYou.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Index()
+        public ActionResult Index(ListUserClass data)
         {
-            return View();
+            using (RewardsForYouEntities db = new RewardsForYouEntities())
+            {
+                IQueryable<RewardsForYouEntities> x = null;
+
+               
+            }
+                return View("Index",data);
         }
+
+
     }
 }
---
+++ RewardsForYou/Models/ListUserClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RewardsForYou.Models
+{
+    public class ListUserClass : RewardsForYouEntities
+    {
+        public ListUserClass()
+            {
+            ResultList = new List<RewardsForYouEntities>();
+            }
+        public List<RewardsForYouEntities> ResultList { get; set; }
+        public new int SerialNumber { get; set; }
+    }
+}
