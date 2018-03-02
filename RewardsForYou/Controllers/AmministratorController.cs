67f8148b20dd3ce4bdb7abcd364db3663133e272
NkompChristian <christian.nkomp@edu.itspiemonte.it>
2018-03-02 15:16:47 +0100

    amministator

---
+++ RewardsForYou/Controllers/AmministratorController.cs
@@ -0,0 +1,89 @@
+﻿using RewardsForYou.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.Ajax.Utilities;
+
+namespace RewardsForYou.Controllers
+{
+    [Authorize]
+    public class AmministratorController : Controller
+    {
+        // GET: Amministrator
+        public ActionResult Index()
+        {
+            using (RewardsForYouEntities db = new RewardsForYouEntities())
+            {
+                int userID = (int)Session["UserID"];
+                return View();
+            }
+        }
+
+
+
+
+        public ActionResult InsertNewUsers()
+        {
+            List<Users> user = new List<Users>();
+            List<Roles> role = new List<Roles>();
+          using (RewardsForYouEntities db = new RewardsForYouEntities())
+                {
+
+                ViewBag.RoleId = new SelectList(role, "Id", "Name");
+
+            }
+            return View();
+        }
+
+        public ActionResult _JsonInsertNewUsers(Users DatiUsers)
+        {
+            if (DatiUsers.UserID != 0 && !string.IsNullOrEmpty(DatiUsers.Serial) && !string.IsNullOrEmpty(DatiUsers.Name) && !string.IsNullOrEmpty(DatiUsers.Surname) && !string.IsNullOrEmpty(DatiUsers.EMail) && DatiUsers.RoleID != 0 && DatiUsers.ManagerUserID != 0)
+            {
+                using (RewardsForYouEntities db = new RewardsForYouEntities())
+                {
+                    db.Users.Add(DatiUsers);
+
+                    db.SaveChanges();
+
+
+
+                }
+            }
+            return Json(new { messaggio = $"Users {DatiUsers.UserID} aggiunto/a con successo" });
+        }
+
+
+        public ActionResult AddRewards()
+        {
+            using (RewardsForYouEntities db = new RewardsForYouEntities())
+            {
+
+                return View();
+            }
+        }
+
+        public ActionResult _JsonAddRewards(Rewards DatiRewards)
+        {
+            if (DatiRewards.RewardsID != 0 && !string.IsNullOrEmpty(DatiRewards.Type) && !string.IsNullOrEmpty(DatiRewards.Description) && DatiRewards.Points !=0 && DatiRewards.Availability !=0)
+            {
+                using (RewardsForYouEntities db = new RewardsForYouEntities())
+                {
+                    db.Rewards.Add(DatiRewards);
+
+                    db.SaveChanges();
+
+
+
+                }
+            }
+            return Json(new { messaggio = $"Rewards {DatiRewards.RewardsID} aggiunto/a con successo" });
+        }
+
+
+
+
+    }
+}
---
+++ RewardsForYou/Controllers/EmployeeDetailController.cs
@@ -0,0 +1,86 @@
+﻿using RewardsForYou.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace RewardsForYou.Controllers
+{
+    [Authorize]
+    public class EmployeeDetailController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(int? UserID)
+        {
+            if (UserID.HasValue)
+            {
+                Session["UserID"] = UserID;
+            }
+            else
+            {
+                UserID = (int)Session["UserID"];
+            }
+            ViewModel viewModel = new ViewModel();
+            MissionModel missionModel = new MissionModel();
+            Users x = null;
+            List<Missions> t = null;
+            List<Tasks> task = new List<Tasks>();
+            List<UsersRewards> u = null;
+            List<Rewards> r = new List<Rewards>();
+
+            if (!UserID.HasValue)
+            {
+                string EMail = ((System.Security.Claims.ClaimsIdentity)HttpContext.GetOwinContext().Authentication.User.Identity).Name;
+                using (RewardsForYouEntities db = new RewardsForYouEntities())
+                {
+
+                    //get the user from the db
+                    x = db.Users.Where(l => l.EMail == EMail).FirstOrDefault();
+                    UserID = x.UserID;
+                }
+            }
+            else
+            {
+                using (RewardsForYouEntities db = new RewardsForYouEntities())
+                {
+
+                    //get the user from the db
+                    x = db.Users.Where(l => l.UserID == UserID).FirstOrDefault();
+                }
+            }
+
+            using (RewardsForYouEntities db = new RewardsForYouEntities())
+            {
+                //get tasks of the user
+                t = db.Missions.Include(m => m.Tasks).Where(l => l.UserID == UserID).ToList();
+
+                foreach (Missions m in t)
+                {
+
+                    task.Add(m.Tasks);
+                }
+
+                //get rewards of the user
+                u = db.UsersRewards.Include(m => m.Rewards).Where(l => l.UserID == UserID).ToList();
+                foreach (UsersRewards re in u)
+                {
+                    r.Add(re.Rewards);
+                }
+                //save the data in the viewModel class
+                viewModel.User = x;
+                viewModel.Mission = task;
+                viewModel.Reward = r;
+            }
+
+            return View(viewModel);
+        }
+
+      
+
+
+    }
+
+
+}
