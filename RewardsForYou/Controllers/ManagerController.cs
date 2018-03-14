a2df3c4dfeb70902b22907d903eab93aa4024e80
NkompChristian <christian.nkomp@edu.itspiemonte.it>
2018-03-14 11:30:22 +0100

    gggg

--- RewardsForYou/Controllers/ManagerController.cs
+++ RewardsForYou/Controllers/ManagerController.cs
@@ -43,17 +43,7 @@ namespace RewardsForYou.Controllers
             
         }
 
-        public ActionResult OrderByName()
-        {
-            using (RewardsForYouEntities db = new RewardsForYouEntities())
-            {
-                var users = from p in db.Users
-                        orderby p.Name ascending
-                        select p;
-
-                return View(users);
-            }
-        }
+      
 
         public ActionResult ViewTask(int? UserID = null)
         {
@@ -124,6 +114,7 @@ namespace RewardsForYou.Controllers
                 }
                 else
                 {
+                    DateTime start = DateTime.Now.Date;
                     Missions mission = new Missions
                     {
 
@@ -134,7 +125,8 @@ namespace RewardsForYou.Controllers
                         StartDate = DateTime.Now,
                         EndDate = task.ExpiryDate,
                         Note = "",
-                        Status = 0
+                        Status = 0,
+                        DesiredEndDate= start.AddMonths(task.TimeSpan)
                     };
 
                     db.Missions.Add(mission);
